9f96c40913d5abe5bddb50c054f538b0e8d11dcc
Jiri Mihal <jiri@mihal.me>
2016-08-03 12:26:16 +0200

    first commit

---
+++ src/Connection.php
@@ -0,0 +1,62 @@
+<?php
+namespace Webiik;
+
+/**
+ * Class Connection
+ * @package     Webiik
+ * @author      Jiří Mihal <jiri@mihal.me>
+ * @copyright   2016 Jiří Mihal
+ * @link        https://github.com/webiik/webiik
+ * @license     MIT
+ */
+class Connection
+{
+    private $conf = [];
+    private $conn = [];
+
+    /**
+     * Add connection configuration
+     * @param $name
+     * @param $dialect
+     * @param $host
+     * @param $dbname
+     * @param $user
+     * @param $pswd
+     * @param $encoding
+     */
+    public function add($name, $dialect, $host, $dbname, $user, $pswd, $encoding)
+    {
+        $this->conf[$name] = [$dialect, $host, $dbname, $user, $pswd, $encoding];
+    }
+
+    /**
+     * Return connection by its name
+     * @param $name
+     * @return \PDO
+     */
+    public function connect($name)
+    {
+        if (!isset($this->conn[$name])) {
+            $this->conn[$name] = new \PDO(
+                $this->conf[$name][0] .
+                ':host=' . $this->conf[$name][1] .
+                ';dbname=' . $this->conf[$name][2],
+                $this->conf[$name][3],
+                $this->conf[$name][4]
+            );
+            $q = $this->conn[$name]->prepare('SET CHARACTER SET ?');
+            $q->execute([$this->conf[$name][5]]);
+        }
+
+        return $this->conn[$name];
+    }
+
+    /**
+     * Unset connection by its name
+     * @param $name
+     */
+    public function disconnect($name)
+    {
+        unset($this->conn[$name]);
+    }
+}
