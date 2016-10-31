9461bbb7485af42c112b2192b56993f29f0cfcec
Jiri Mihal <jiri@mihal.me>
2016-10-31 19:18:36 +0100

    under development

--- example/private/app/controllers/Login.php
+++ example/private/app/controllers/Login.php
@@ -125,35 +125,75 @@ class Login
 
         $http = new Http();
         $token = new Token();
-        $oauth = new OAuth1Client($http, $token);
+
+        $oauth = new OAuth2Client($http);
 
         // Your callback URL after authorization
-        $oauth->setCallbackUrl('https://localhost/skeletons/webiik/example/login/');
+        $oauth->setRedirectUri('https://localhost/skeletons/webiik/example/login/');
 
         // API end points
-        $oauth->setReqestTokenUrl('https://api.twitter.com/oauth/request_token');
-        $oauth->setAuthorizeUrl('https://api.twitter.com/oauth/authenticate');
-        $oauth->setAccessTokenUrl('https://api.twitter.com/oauth/access_token');
+        $oauth->setAuthorizeUrl('https://www.facebook.com/v2.8/dialog/oauth');
+        $oauth->setAccessTokenUrl('https://graph.facebook.com/v2.8/oauth/access_token');
+        $oauth->setValidateTokenUrl('https://graph.facebook.com/debug_token');
 
         // API credentials
-        $oauth->setConsumerSecret('rZcebQTj3S01dnVPNeYXwctmxmhvZfG6WdSN9KcCoLzCGrB1g0');
-        $oauth->setConsumerKey('YgMxXP37WfVhJT6t1iC9f5PkB');
+        $oauth->setClientId('1789792224627518');
+        $oauth->setClientSecret('04f626a495ae205185c7271c3d6a7d9a');
 
         // Make API calls
 
         // Log in a user
-        if (!isset($_GET['oauth_verifier'])) {
-            $requestTokenData = $oauth->getRequestTokenData();
-            if (isset($requestTokenData['oauth_token'])) {
-                $oauth->redirectToLoginUrl($requestTokenData['oauth_token']);
+        $url = $oauth->getLoginUrl(
+            [
+                'email',
+            ],
+            'code'
+        );
+        echo '<a href="' . $url . '">FB login</a>';
+
+        // Get Access token
+        if (isset($_GET['code'])) {
+
+            $data = $oauth->getAccessTokenByCode($_GET['code'], 'GET');
+            print_r($data);
+
+            if (isset($data['access_token'])) {
+                $info = $oauth->getTokenInfo($data['access_token'], $data['access_token'], 'GET');
+                print_r($info);
             }
         }
 
-        $accessToken = $oauth->getAccessTokenData();
-
-        print_r($accessToken);
-
         exit;
+
+//        $oauth = new OAuth1Client($http, $token);
+//
+//        // Your callback URL after authorization
+//        $oauth->setCallbackUrl('https://localhost/skeletons/webiik/example/login/');
+//
+//        // API end points
+//        $oauth->setReqestTokenUrl('https://api.twitter.com/oauth/request_token');
+//        $oauth->setAuthorizeUrl('https://api.twitter.com/oauth/authenticate');
+//        $oauth->setAccessTokenUrl('https://api.twitter.com/oauth/access_token');
+//
+//        // API credentials
+//        $oauth->setConsumerSecret('rZcebQTj3S01dnVPNeYXwctmxmhvZfG6WdSN9KcCoLzCGrB1g0');
+//        $oauth->setConsumerKey('YgMxXP37WfVhJT6t1iC9f5PkB');
+//
+//        // Make API calls
+//
+//        // Log in a user
+//        if (!isset($_GET['oauth_verifier'])) {
+//            $requestTokenData = $oauth->getRequestTokenData();
+//            if (isset($requestTokenData['oauth_token'])) {
+//                $oauth->redirectToLoginUrl($requestTokenData['oauth_token']);
+//            }
+//        }
+//
+//        $accessToken = $oauth->getAccessTokenData();
+//
+//        print_r($accessToken);
+//
+//        exit;
 
 //        // GOOGLE
 //
@@ -294,118 +334,118 @@ class Login
 //
 //        }
 //
-        // TWITTER
-
-        // API SETTINGS
-        $consumerSecret = 'rZcebQTj3S01dnVPNeYXwctmxmhvZfG6WdSN9KcCoLzCGrB1g0';
-        $consumerKey = 'YgMxXP37WfVhJT6t1iC9f5PkB';
-        $oauth_signature_method = 'HMAC-SHA1';
-
-        // My Twitter Auth
-        if (!isset($_GET['oauth_verifier'])) {
-
-            // STEP 1 - TWITTER OBTAINING A REQUEST TOKEN
-            $callbackUrl = 'https://localhost/skeletons/webiik/example/login/';
-            $url = 'https://api.twitter.com/oauth/request_token';
-
-            // Data we will send
-            $data = [
-                'oauth_callback' => $callbackUrl,
-                'oauth_consumer_key' => $consumerKey,
-                'oauth_signature_method' => $oauth_signature_method,
-                'oauth_timestamp' => time(),
-                'oauth_nonce' => $token->generate(3),
-                'oauth_version' => '1.0',
-            ];
-
-            // Sort data alphabetically, because Twitter requires that
-            ksort($data);
-
-            // Generate signature and add it to data array
-            $signData = 'POST&' . urlencode($url) . '&' . urlencode(http_build_query($data));
-            $secret = '';
-            $signKey = urlencode($consumerSecret) . '&' . urlencode($secret);
-            $data['oauth_signature'] = base64_encode(hash_hmac('sha1', $signData, $signKey, true));
-
-            // Prepare http headers from data
-            $httpHeaders = [];
-            foreach ($data as $key => $value) {
-                $httpHeaders[] = urlencode($key) . '="' . urlencode($value) . '"';
-            }
-
-            // Add OAuth header with all data
-            $httpHeaders = 'Authorization: OAuth ' . implode(', ', $httpHeaders);
-
-            // Send post request to Twitter API with http headers and data
-            $res = $http->post($url, ['httpHeaders' => [$httpHeaders]], []);
-
-            // If we got some error, show error message and stop
-            if ($res['status'] != 200) {
-                echo $res['err'];
-                exit;
-            }
-
-            // Prepare data for step 2 and 3 from Twitter's response
-            parse_str($res['body'], $res);
-//            $oauth_callback_confirmed = $res['oauth_callback_confirmed'];
-            $oauth_request_token = $res['oauth_token'];
-
-            // Store oauth_token_secret into session, we will need it in step 3
-//            $this->sessions->setToSession('oauth_token_secret', $res['oauth_token_secret']);
-//            $this->sessions->setToSession('oauth_token', $oauth_request_token);
-
-            // STEP 2 - REDIRECTING THE USER TO TWITTER LOGIN
-            header('HTTP/1.1 302 Found');
-            header('Location: https://api.twitter.com/oauth/authenticate?oauth_token=' . urlencode($oauth_request_token));
-        }
-
-        // STEP 3 - CONVERTING THE REQUEST TOKEN TO AN ACCESS TOKEN
-        $url = 'https://api.twitter.com/oauth/access_token';
-        $oauth_token = $_GET['oauth_token'];
-        $oauth_verifier = $_GET['oauth_verifier'];
-
-        // Data we will send
-        $data = [
-            'oauth_consumer_key' => $consumerKey,
-            'oauth_nonce' => $token->generate(3),
-            'oauth_signature_method' => $oauth_signature_method,
-            'oauth_timestamp' => time(),
-            'oauth_token' => $oauth_token,
-            'oauth_version' => '1.0',
-        ];
-
-        // Sort data alphabetically, because Twitter requires that
-        ksort($data);
-
-        // Generate signature and add it to data array
-        $signData = 'POST&' . urlencode($url) . '&' . urlencode(http_build_query($data));
-        $secret = '';
-        $signKey = urlencode($consumerSecret) . '&' . urlencode($secret);
-        $data['oauth_signature'] = base64_encode(hash_hmac('sha1', $signData, $signKey, true));
-
-        // Prepare http headers from data
-        $httpHeaders = [];
-        foreach ($data as $key => $value) {
-            $httpHeaders[] = urlencode($key) . '="' . urlencode($value) . '"';
-        }
-
-        // Add OAuth header with all data
-        $httpHeaders = ['Authorization: OAuth ' . implode(', ', $httpHeaders)];
-        $httpHeaders[] = 'Content-Length: ' . strlen('oauth_verifier=' . urlencode($oauth_verifier));
-        $httpHeaders[] = 'Content-Type: application/x-www-form-urlencoded';
-
-        // Add oauth_verifier to POST data
-        $postData = ['oauth_verifier' => $oauth_verifier];
-
-        // Send post request to Twitter API with http headers and data
-        $res = $http->post($url, ['httpHeaders' => $httpHeaders], $postData);
-
-        // If we got some error, show error message and stop
-        if ($res['status'] != 200) {
-            print_r($res);
-            exit;
-        }
-
-        print_r($res);
+//        // TWITTER
+//
+//        // API SETTINGS
+//        $consumerSecret = 'rZcebQTj3S01dnVPNeYXwctmxmhvZfG6WdSN9KcCoLzCGrB1g0';
+//        $consumerKey = 'YgMxXP37WfVhJT6t1iC9f5PkB';
+//        $oauth_signature_method = 'HMAC-SHA1';
+//
+//        // My Twitter Auth
+//        if (!isset($_GET['oauth_verifier'])) {
+//
+//            // STEP 1 - TWITTER OBTAINING A REQUEST TOKEN
+//            $callbackUrl = 'https://localhost/skeletons/webiik/example/login/';
+//            $url = 'https://api.twitter.com/oauth/request_token';
+//
+//            // Data we will send
+//            $data = [
+//                'oauth_callback' => $callbackUrl,
+//                'oauth_consumer_key' => $consumerKey,
+//                'oauth_signature_method' => $oauth_signature_method,
+//                'oauth_timestamp' => time(),
+//                'oauth_nonce' => $token->generate(3),
+//                'oauth_version' => '1.0',
+//            ];
+//
+//            // Sort data alphabetically, because Twitter requires that
+//            ksort($data);
+//
+//            // Generate signature and add it to data array
+//            $signData = 'POST&' . urlencode($url) . '&' . urlencode(http_build_query($data));
+//            $secret = '';
+//            $signKey = urlencode($consumerSecret) . '&' . urlencode($secret);
+//            $data['oauth_signature'] = base64_encode(hash_hmac('sha1', $signData, $signKey, true));
+//
+//            // Prepare http headers from data
+//            $httpHeaders = [];
+//            foreach ($data as $key => $value) {
+//                $httpHeaders[] = urlencode($key) . '="' . urlencode($value) . '"';
+//            }
+//
+//            // Add OAuth header with all data
+//            $httpHeaders = 'Authorization: OAuth ' . implode(', ', $httpHeaders);
+//
+//            // Send post request to Twitter API with http headers and data
+//            $res = $http->post($url, ['httpHeaders' => [$httpHeaders]], []);
+//
+//            // If we got some error, show error message and stop
+//            if ($res['status'] != 200) {
+//                echo $res['err'];
+//                exit;
+//            }
+//
+//            // Prepare data for step 2 and 3 from Twitter's response
+//            parse_str($res['body'], $res);
+////            $oauth_callback_confirmed = $res['oauth_callback_confirmed'];
+//            $oauth_request_token = $res['oauth_token'];
+//
+//            // Store oauth_token_secret into session, we will need it in step 3
+////            $this->sessions->setToSession('oauth_token_secret', $res['oauth_token_secret']);
+////            $this->sessions->setToSession('oauth_token', $oauth_request_token);
+//
+//            // STEP 2 - REDIRECTING THE USER TO TWITTER LOGIN
+//            header('HTTP/1.1 302 Found');
+//            header('Location: https://api.twitter.com/oauth/authenticate?oauth_token=' . urlencode($oauth_request_token));
+//        }
+//
+//        // STEP 3 - CONVERTING THE REQUEST TOKEN TO AN ACCESS TOKEN
+//        $url = 'https://api.twitter.com/oauth/access_token';
+//        $oauth_token = $_GET['oauth_token'];
+//        $oauth_verifier = $_GET['oauth_verifier'];
+//
+//        // Data we will send
+//        $data = [
+//            'oauth_consumer_key' => $consumerKey,
+//            'oauth_nonce' => $token->generate(3),
+//            'oauth_signature_method' => $oauth_signature_method,
+//            'oauth_timestamp' => time(),
+//            'oauth_token' => $oauth_token,
+//            'oauth_version' => '1.0',
+//        ];
+//
+//        // Sort data alphabetically, because Twitter requires that
+//        ksort($data);
+//
+//        // Generate signature and add it to data array
+//        $signData = 'POST&' . urlencode($url) . '&' . urlencode(http_build_query($data));
+//        $secret = '';
+//        $signKey = urlencode($consumerSecret) . '&' . urlencode($secret);
+//        $data['oauth_signature'] = base64_encode(hash_hmac('sha1', $signData, $signKey, true));
+//
+//        // Prepare http headers from data
+//        $httpHeaders = [];
+//        foreach ($data as $key => $value) {
+//            $httpHeaders[] = urlencode($key) . '="' . urlencode($value) . '"';
+//        }
+//
+//        // Add OAuth header with all data
+//        $httpHeaders = ['Authorization: OAuth ' . implode(', ', $httpHeaders)];
+//        $httpHeaders[] = 'Content-Length: ' . strlen('oauth_verifier=' . urlencode($oauth_verifier));
+//        $httpHeaders[] = 'Content-Type: application/x-www-form-urlencoded';
+//
+//        // Add oauth_verifier to POST data
+//        $postData = ['oauth_verifier' => $oauth_verifier];
+//
+//        // Send post request to Twitter API with http headers and data
+//        $res = $http->post($url, ['httpHeaders' => $httpHeaders], $postData);
+//
+//        // If we got some error, show error message and stop
+//        if ($res['status'] != 200) {
+//            print_r($res);
+//            exit;
+//        }
+//
+//        print_r($res);
     }
 }
