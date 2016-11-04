da989608e5afd09a2605710de15dffa7f904b031
Jiri Mihal <jiri@mihal.me>
2016-11-04 19:55:56 +0100

    update Removed unused lines

--- example/index.php
+++ example/index.php
@@ -1,6 +1,5 @@
 <?php
 //$before = microtime(true);
-// Uncomment for memory usage testing
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/private/vendor/autoload.php';
@@ -11,8 +10,7 @@ require __DIR__ . '/private/app/bootstrap.php';
 //echo '<br/><br/>Peak memory usage: ' . (memory_get_peak_usage() / 1000000) . ' MB';
 //echo '<br/>End memory usage: ' . (memory_get_usage() / 1000000) . ' MB';
 
-// Uncomment for getting of all defined PHP vars
-//print_r(get_defined_vars());
-
 //$after = microtime(true);
-//echo ' < br /><br />Execution time: '. ($after-$before) . ' sec';
+//echo '<br/><br/>Execution time: '. ($after-$before) . ' sec';
+
+//print_r(get_defined_vars());
