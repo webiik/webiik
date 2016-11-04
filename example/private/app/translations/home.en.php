3255f209d46718bd12bdf5dcfdfd32aa7fe9d4d7
Jiri Mihal <jiri@mihal.me>
2016-11-04 20:01:55 +0100

    update Translation example

--- example/private/app/translations/home.en.php
+++ example/private/app/translations/home.en.php
@@ -1,14 +1,6 @@
 <?php
 /**
  * This translation will be available only for current route and lang.
- *
- * Special keys:
- * 'seo' => [
- *      'title' => 'Content of html title tag',
- *      'desc' => 'Content of meta description',
- *      'kw' => 'Content of meta keywords',
- * ]
- *
  */
 return [
     'seo' => [
@@ -18,8 +10,4 @@ return [
     ],
     't1' => 'Today is {timeStamp, date}, the time is {timeStamp, time}.',
     't2' => '{numCats, plural, =0 {Any cat have} =1 {One cat has} =2+ {{numCats} cats have}} birthday.',
-    't3' => 'This car costs {price, currency, currency}.',
-    't4' => '{gender, select, =male {He} =female {She}} is {gender}. {gender, select, =male {Males like beer.} =female {Females like wine.}}',
-    't5' => 'Maximal allowed speed is {speed, conv, kmh, mph, su}.',
-    't6' => 'Hello {{ test }}!',
 ];
