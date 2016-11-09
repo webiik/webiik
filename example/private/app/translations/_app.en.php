75281cfec961d3e2e81e05f983a33b72948db233
Jiri Mihal <jiri@mihal.me>
2016-11-09 19:36:44 +0100

    update Skeleton example content

--- example/private/app/translations/_app.en.php
+++ example/private/app/translations/_app.en.php
@@ -1,28 +1,32 @@
 <?php
 /**
- * All shared translations of current lang.
- *
- * Special key:
- * 'routes' - here you assign translated URIs to route definition '/routes/routes.php' by route name
- *
- * Note: Only assigned routes in current/fallback(s) lang(s) will be available in current lang.
+ * App wide translations for current language.
+ * These translations will be available on every page. Great for header, footer etc.
+ * Don't place here translations for separate pages!
  */
 return [
-    // Todo: Nav translation
+    // Special key 'routes' - here you write URI translations of routes.
+    // Only routes with translated URIs will be active.
     'routes' => [
         'home' => '/',
         'login' => '/login',
+        'login-oauth1' => '/login-oauth1',
+        'signup' => '/signup',
+        'forgot' => '/forgot-password',
+        'logout' => '/logout',
         'account' => '/account',
+        'admin' => '/admin',
     ],
-    'login' => [
-        'msg-ok' => 'Welcome {name} to your account!',
-        'msg-err-1' => 'Wrong email.',
-        'msg-err-2' => 'Wrong password.',
-        'msg-err-3' => 'Active account in confirmation email.',
-        'msg-err-4' => 'Your account has been banned.',
-        'link-activation' => 'Re-send activation email.',
-    ],
-    'other' => [
-        'key' => 'val',
+    'nav' => [
+        // 0 - always show nav item
+        // 1 - show nav item only when user is not logged in
+        // 2 - show nav item only when user is logged in
+        'home' => ['Home page', 0],
+        'login' => ['Log in', 1],
+        'signup' => ['Sign up', 1],
+        'forgot' => ['Forgot password?', 1],
+        'account' => ['My account', 2],
+        'admin' => ['Admin', 2],
+        'logout' => ['Logout', 2],
     ],
 ];
