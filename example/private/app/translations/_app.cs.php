7df07e878a6743244e9f42b3f6ecdb22f0011de2
Jiri Mihal <jiri@mihal.me>
2016-11-09 19:36:22 +0100

    update Skeleton example content

--- example/private/app/translations/_app.cs.php
+++ example/private/app/translations/_app.cs.php
@@ -3,10 +3,18 @@ return [
     'routes' => [
         'home' => '/',
         'login' => '/prihlaseni',
+        'login-oauth1' => '/login-oauth1',
+        'signup' => '/registrace',
+        'forgot' => '/zapomenute-heslo',
         'account' => '/ucet',
+        'admin' => '/admin',
     ],
-    'other' => [
-        'a' => ['b' => 'c'],
-        'nav-home' => 'Úvodní stránka',
+    'nav' => [
+        'home' => ['Úvodní stránka', 0],
+        'login' => ['Přihlášení', 1],
+        'signup' => ['Registrace', 1],
+        'forgot' => ['Zapomenuté heslo', 1],
+        'account' => ['Můj účet', 2],
+        'admin' => ['Admin', 2],
     ],
 ];
