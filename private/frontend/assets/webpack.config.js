b459ac13f641906eecf268e55f06e05ab1353437
Jiri Mihal <jiri@mihal.me>
2021-07-02 15:23:44 +0200

    update The latest version of Webpack and packages, add Support for IE10+

--- private/frontend/assets/webpack.config.js
+++ private/frontend/assets/webpack.config.js
@@ -2,45 +2,59 @@ const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const {StatsWriterPlugin} = require("webpack-stats-plugin");
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin');
+const {StatsWriterPlugin} = require('webpack-stats-plugin');
 let configs = [];
 
-// Entries
+// Specify your Webpack entries here...
 const entries = {
-    // The following JS will be loaded only on client
+    /*
+     * Specification of entry names:
+     * 1) Entry names starting with a single underscore are automatically loaded in every route.
+     * 2) Entry names with the same name as the route name are automatically loaded only within a specific route.
+     * 3) Entry names with a name other than 1) 2) aren't automatically loaded.
+     */
     'client': {
-        // App wide JS
-        // _app.js is loaded in every route via getJS in _app.twig
+        /*
+         * The following entries will be transpiled for the client and loaded
+         * via getJS function in _app.twig after the main content block.
+         * If you need to load an entry before the main content block,
+         * just add a single underscore at the end of the entry name eg. `home_`.
+         *
+         * Transpiled files are located in `/public/assets`
+         */
         '_app': [
             './js/_app.ts',
         ],
-        // Route specific JS
-        // Webpack key MUST be the same as the route name in route configuration file
-        // {key}.js is loaded in route via getJS in _app.twig
         'home': [
             './js/home.ts',
         ],
     },
-    // The following JS will be loaded on server and client
-    // This feature is intended to pre-render UI components
     'iso': {
-        // Route specific JS
-        // Webpack key MUST be the same as the route name in route configuration file and MUST include '-iso' suffix
+        /*
+         * The following entries will transpiled for the client and for the server.
+         * Client entries will be loaded via getJS function in _app.twig
+         * before the main content block. Server entries can be loaded on
+         * your demand, for example using the webiik/ssr package.
+         *
+         * Transpiled files are located in `/public/assets` and `/private/frontend/assets/build/server`
+         *
+         * !!! NOTICE !!!
+         * All entry names defined in `iso` block MUST include '-iso' suffix
+         */
         'home-iso': [
            './js/home-iso.ts',
         ],
     },
 };
 
-
-// Webpack configuration for client
+// Webpack configuration for the client
 const client = (env, argv) => {
     return {
         entry: Object.assign(entries.client, entries.iso),
         output: {
-            filename: 'js/[name].js',
-            chunkFilename: 'js/[name].js',
+            filename: 'js/[name].[contenthash].js',
+            chunkFilename: 'js/[name].[contenthash].js',
             path: path.resolve(__dirname + '/../../../public/assets/')
         },
         resolve: {
@@ -54,7 +68,12 @@ const client = (env, argv) => {
             // Split all shared code to chunk files vendors~{key}.js
             splitChunks: {
                 chunks: 'all',
-            }
+                cacheGroups: {
+                    defaultVendors: {
+                        filename: 'js/vendors~[name].[contenthash].js',
+                    },
+                },
+            },
         },
         plugins: [
             new webpack.DefinePlugin({
@@ -62,11 +81,13 @@ const client = (env, argv) => {
                 'WEBIIK_DEBUG': argv.mode === 'development',
             }),
             new MiniCssExtractPlugin({
-                filename: 'css/[name].css',
+                filename: 'css/[name].[contenthash].css',
             }),
-            new CopyPlugin([
-                {from: 'img', to: 'img'}
-            ]),
+            new CopyPlugin({
+                patterns: [
+                    {from: 'img', to: 'img'},
+                ],
+            }),
             // Generate helper file assets.json
             // Thanks to this file Webiik knows which *.js files must be loaded in specific route
             new StatsWriterPlugin({
@@ -86,12 +107,12 @@ const client = (env, argv) => {
                             if (!addedAssets[baseEntryPoint]) {
                                 addedAssets[baseEntryPoint] = {};
                             }
-                            if (!addedAssets[baseEntryPoint][data.entrypoints[entrypoint].assets[index]]) {
-                                let asset = data.entrypoints[entrypoint].assets[index];
+                            if (!addedAssets[baseEntryPoint][data.entrypoints[entrypoint].assets[index].name]) {
+                                let asset = data.entrypoints[entrypoint].assets[index].name;
                                 if (asset.match(/\.css$/)) {
                                     assetsCss.push(asset);
                                 } else {
-                                    if (data.entrypoints[entrypoint].assets[index] === 'js/runtime.js' && baseEntryPoint !== '_app') {
+                                    if (data.entrypoints[entrypoint].assets[index].name.match(/js\/runtime\.\w+\.js/) && baseEntryPoint !== '_app') {
                                         // Don't push runtime.js to different entry point than _app
                                     } else {
                                         assetsJs.push(asset);
@@ -108,10 +129,34 @@ const client = (env, argv) => {
                     return JSON.stringify(resJson, null, 2);
                 }
             }),
-            new CleanWebpackPlugin({
-                dry: false,
-                cleanStaleWebpackAssets: false,
-            }),
+            new RemovePlugin({
+                // Always remove those assets before build
+                before: {
+                    include: [
+                        'build/server',
+                        '../../../public/assets/img',
+                        '../../../public/assets/css',
+                        '../../../public/assets/js',
+                    ],
+                    test: [
+                        {
+                            folder: 'build',
+                            method: (absPath) => new RegExp(/assets\.json$/).test(absPath)
+                        }
+                    ],
+                    allowRootAndOutside: true
+                },
+                // Remove *.LICENSE.txt files generated by TerserPlugin after build
+                after: {
+                    test: [
+                        {
+                            folder: '../../../public/assets/js',
+                            method: (absPath) => new RegExp(/\.LICENSE\.txt$/).test(absPath)
+                        }
+                    ],
+                    allowRootAndOutside: true
+                }
+            })
         ],
         module: {
             rules: [
@@ -136,7 +181,6 @@ const client = (env, argv) => {
                     exclude: /node_modules/,
                     options: {
                         name: '[path][name].[ext]',
-                        // outputPath: './',
                     },
                     loader: 'file-loader',
                 },
@@ -145,7 +189,6 @@ const client = (env, argv) => {
                     exclude: /node_modules/,
                     options: {
                         name: '[path][name].[ext]',
-                        // outputPath: './',
                     },
                     loader: 'file-loader',
                 }
@@ -161,13 +204,13 @@ const client = (env, argv) => {
     }
 };
 
-// Webpack configuration for server (PHP v8js or NodeJS)
+// Webpack configuration for the server (PHP V8JS or NodeJS)
 const server = (env, argv) => {
     return {
         entry: entries.iso,
         output: {
-            filename: 'server/[name].js',
-            chunkFilename: 'server/[name].js',
+            filename: 'server/[name].[contenthash].js',
+            chunkFilename: 'server/[name].[contenthash].js',
             path: path.resolve(__dirname + '/build/')
         },
         resolve: {
@@ -178,10 +221,18 @@ const server = (env, argv) => {
                 // Define global variable(s) accessible in your JS code
                 'WEBIIK_DEBUG': argv.mode === 'development',
             }),
-            new CleanWebpackPlugin({
-                dry: false,
-                cleanStaleWebpackAssets: false,
-            }),
+            new RemovePlugin({
+                // Remove *.LICENSE.txt files generated by TerserPlugin during build
+                after: {
+                    test: [
+                        {
+                            folder: 'build/server',
+                            method: (absPath) => new RegExp(/\.LICENSE\.txt$/).test(absPath)
+                        }
+                    ],
+                    allowRootAndOutside: true
+                }
+            })
         ],
         module: {
             rules: [
@@ -228,6 +279,45 @@ const server = (env, argv) => {
     }
 };
 
+// Webpack configuration for old browsers
+const polyfill = (env, argv) => {
+    return {
+        entry: {
+            'polyfills': ['./js/polyfills.ts']
+        },
+        output: {
+            filename: 'js/polyfills.js',
+            path: path.resolve(__dirname + '/../../../public/assets/')
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                // Define global variable(s) accessible in your JS code
+                'WEBIIK_DEBUG': argv.mode === 'development',
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: 'ts-loader',
+                },
+            ]
+        },
+        externals: {
+            // If you need to pass external data to your *.ts files, you can
+            // do it via variable webpackData. Just define this variable before
+            // calling getJS function in your (Twig) template and import it in
+            // your *.ts file using `import webpackData from 'webpackData';`
+            webpackData: 'webpackData'
+        },
+    }
+};
+
 if (Object.keys(entries.client).length) configs.push(client);
 if (Object.keys(entries.iso).length) configs.push(server);
+configs.push(polyfill);
 module.exports = configs;
